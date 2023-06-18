3d4c8351b0bcf15e7db1127d1dc0fdbf8cf79607
Mahdi Bozorg <102772767+Mehtisup@users.noreply.github.com>
2023-06-19 00:24:44 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,123 @@
+function switchFunction() {
+    var myBody = document.getElementById("body");
+    myBody.classList.toggle("toChange");
+}
+
+window.onscroll = function() {scrollFunction()};
+
+var menu = window.pageYOffset;
+
+function scrollFunction() {
+    
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        document.getElementById("sButton").style.display = "block";
+    } else {
+        document.getElementById("sButton").style.display = "none";
+    }
+
+    var myMenu = window.pageYOffset;
+
+    if (menu > myMenu) {
+        document.getElementById("mySocial").style.transform = "translateY(0)";
+        
+    } else {
+        document.getElementById("mySocial").style.transform = "translateY(-60px)"
+    }
+    menu = myMenu;
+    
+    
+}
+
+
+function sFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+function myClock() {
+    var myWatch = new Date();
+    
+    var h = myWatch.getHours();
+    var m = myWatch.getMinutes();
+    var s = myWatch.getSeconds();
+
+    m = checkTime(m);
+    s = checkTime(s);
+    setTimeout(myClock, 1000);
+    document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i}
+    return i;
+}
+
+myClock();
+
+function listFunction() {
+    document.getElementById("myLayer").style.display = "block";;
+    document.getElementById("myBar").style.display = "none";
+    document.getElementById("myClose").style.display = "block";
+}
+
+function closeFunction() {
+    document.getElementById("myLayer").style.display = "none";
+    document.getElementById("myClose").style.display = "none";
+    document.getElementById("myBar").style.display = "block";
+}
+
+function aboutFunction() {
+    document.getElementById("myAbout").style.display = "block";
+}
+
+function cAbout() {
+    document.getElementById("myAbout").style.display = "none";
+}
+
+function pFunction() {
+    document.getElementById("myGreen").style.width = "100%";
+    document.getElementById("imGreen").style.transform = "translateY(0)";
+    document.getElementById("imGreen").style.opacity = "1";
+}
+
+function cGreen() {
+    document.getElementById("myGreen").style.width = "0%";
+    document.getElementById("imGreen").style.opacity = "0";
+    document.getElementById("imGreen").style.transform = "translateY(0px)"
+}
+
+function websiteFunction() {
+    alert("Mahdi Bozorg")
+}
+
+function cWebsite() {
+    document.getElementById("myWebsite").style.width = "0";
+    document.getElementById("mw").style.opacity = "0";
+    document.getElementById("mw").style.transform = "scale(0)";
+}
+
+function serviceFunction() {
+    document.getElementById("myDark").style.dispaly = "block";
+    
+}
+function darkFunction() {
+    document.getElementById("myDark").style.display = "none";
+    
+}
+
+function technologyFunction() {
+    document.getElementById("learning").style.display = "block";
+}
+
+function cTechnology() {
+    document.getElementById("learning").style.display = "none";
+}
+
+function storeFunction() {
+    document.getElementById("darkSide").style.display = "block";
+}
+
+function sideFunction() {
+    document.getElementById("darkSide").style.display = "none";
+}
